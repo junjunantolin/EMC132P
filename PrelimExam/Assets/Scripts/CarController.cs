a22eb6f0f7e74b07f4fd5216a2eb3779370fe83c
junjunantolin <antolin_e@yahoo.com>
2020-08-30 13:20:27 +0800

    first

---
+++ PrelimExam/Assets/Scripts/CarController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public float gravityMutliplier;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb  = GetComponent<Rigidbody>(); 
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        Turn();
+        Fall();
+    }
+    void Move()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(Vector3.forward * speed * 10);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(-Vector3.forward * speed * 10);
+        }
+        Vector3 localvelocity = transform.InverseTransformDirection(rb.velocity);
+        localvelocity.x = 0;
+        rb.velocity = transform.TransformDirection(localvelocity);
+    }
+    void Turn()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(Vector3.up * turnSpeed * 10);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(-Vector3.up * turnSpeed* 10);
+        }
+    }
+    void Fall()
+    {
+        rb.AddForce(Vector3.down * gravityMutliplier * 10);
+    } 
+}
